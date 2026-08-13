5d472d3a67c15386baf6310a0af26a89b0ab1639
akos00221 <109477064+akos00221@users.noreply.github.com>
2026-08-13 14:52:04 +0200

    Modify SSD1309 screen update horizontal mode

--- I2C,SPI/6_ssd1309mpu6050/DisplaySensorDataOnScreen/Core/Src/SSD1309.c
+++ I2C,SPI/6_ssd1309mpu6050/DisplaySensorDataOnScreen/Core/Src/SSD1309.c
@@ -1,9 +1,14 @@
 #include "SSD1309.h"
 #include "main.h"
 
-static void SSD1309UpdateScreenHOR(SPI_HandleTypeDef *hspi, uint8_t screen[static 128]);
-static void SSD1309UpdateScreenVER(SPI_HandleTypeDef *hspi, uint8_t screen[static 128]);
-static void SSD1309UpdateScreenPAGE(SPI_HandleTypeDef *hspi, uint8_t screen[static 128]);
+static void SSD1309UpdateScreenHOR(SPI_HandleTypeDef *hspi, uint8_t screen[static 8*128]){
+	HAL_GPIO_WritePIN(pinmap->cs.GPIOx, pinmap->cs.GPIO_Pin, GPIO_PIN_RESET);
+	HAL_Delay(50);
+	HAL_SPI_Transmit_DMA(hspi, screen, 8*128);
+	HAL_GPIO_WritePIN(pinmap->cs.GPIOx, pinmap->cs.GPIO_Pin, GPIO_PIN_SET);
+}
+static void SSD1309UpdateScreenVER(SPI_HandleTypeDef *hspi, uint8_t screen[static 8*128]);
+static void SSD1309UpdateScreenPAGE(SPI_HandleTypeDef *hspi, uint8_t screen[static 8*128]);
 void SSD1309ResetDevice(SPI_HandleTypeDef *hspi, SSD1309_PinMap *pinmap){
 	  HAL_GPIO_WritePin(pinmap->res.GPIOx, pinmap->res.GPIO_Pin, GPIO_PIN_RESET);
 	  HAL_Delay(50);
